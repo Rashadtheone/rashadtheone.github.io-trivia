a7c7793380f8a49404dead95ced99f776c79e4ed
Rashad Hammonds <rhammonds@yearup.org>
2017-12-12 16:36:37 -0500

    yeah we got more functionaility

--- js/script.js
+++ js/script.js
@@ -1,48 +1,68 @@
+// main Array of objects
+var aqNaruto = [
+  {
+    questionN: 1,
+    q: 'who is naruto?',
+    a: ['./imgs/q11.png', './imgs/q12.png', './imgs/q13.png', './imgs/q14.png'],
+    answer: './imgs/q13.png'
+  },
+  {
+    questionN: 2,
+    q: 'who is sasuke?',
+    a: ['../imgs/q15.png', '../imgs/q16.png', '../imgs/q17.png', '../imgs/q18.png'],
+    answer: '../imgs/q18.png'
+  }
+    // set the variable index at 0, for the question
+
+]
+// the index of the array
+var index = 0
+// The end of my global scope
+// Enables Jquery on the document
 $(document).ready(function () {
+    // onlcik buut that allows user to start the game
   $('#start').click(function (e) {
+      // preventing button default actions
     e.preventDefault()
+    // labels the input variable as the textbox for amount of players.
     var input = ($('#players')).val()
-    let newGame = new GameStart(input)
+    // outputs the amount of players in the game based on conditional answers.
+    if (input === '1') {
+    // Created a variable for player to be appended to the DOM
+      var $player1 = $("<div class='player-chioce'> <h1>Player 1</h1> <h2 id = 'p1score'>Score: <span id ='numberV'>0</span></h2></div>")
+      $('#player-stats').append($player1)
+      // call to start game function
+      startGame()
+    }// outputs the amount of players in the game based on conditional answers.
+    else if (input === '2') {
+        // creates two players on the DOM!
+      $player1 = $("<div class='player-choice'> <h1>Player 1</h1> <h2 id = 'p1score'>Score: 0</h2></div>")
+      var $player2 = $("<div class='player-choice'> <h1>Player 2</h1> <h2 id = 'p1score'>Score: 0</h2></div>")
+      startGame()
+      $('#player-stats').append($player1, $player2)
+    } else {
+      alert('too many players, it can only be two.')
+    }
   })
-//     $('#brush').css('background', input.val())
-//   })
-
-//   var i
-//   for (i = 0; i < 8000; i++) {
-//     $('body').append("<div class = 'square'> </div>")
-//   }
-//   $('.square').mouseover(function () {
-//     $(this).css('background', input.val())
-//
-
-  class GameStart {
-    constructor (newGame) {
-      this.newGame = newGame.value
-      if (newGame === '1') {
-        var $player1 = $( "<div class='player-chioce'> <h1>Player 1</h1> <h2>Score: 0</h2></div>" )
-        $("#player-stats").append($player1)
-      } else {
-        var $player1 = $( "<div class='player-choice'> <h1>Player 1</h1> <h2>Score: 0</h2></div>" )
-        var $player2 = $( "<div class='player-choice'> <h1>Player 2</h1> <h2>Score: 0</h2></div>" )
-        $("#player-stats").append($player1, $player2)
-      }
+  function startGame () {
+    $('#question-board').append(`<div class = 'active-question'> ${aqNaruto[index].q}</div>`)
+    for (var i = 0; i < aqNaruto[index].a.length; i++) {
+      $('#answer-board').append(`<div><img class ='active-choices' src = "${aqNaruto[index].a[i]}" /></div>`)
     }
-     startGame (newGame) {
-        var players = this.newGame
-         for(players; players < 1; players++) {
-            cosnole.log('bacon')
-         }
-     }
-    }
-  class Questions {
 
-}
-
-  class Answers extends Questions {
-
-}
-
-  class Result {
-
-}
+    $('.active-choices').on('click', function () {
+        var answer = `${aqNaruto[index].answer}`
+        var userInput = $(this).attr('src')
+        if (userInput === answer) {
+          var score$ = $('#numberV').text()
+            var $newScore = parseInt(score$)
+            console.log($newScore)
+            $('#numberV').text($newScore += 1)
+        
+            console.log('your right')
+        } else {
+            alert('your wrong')
+        }
+ })
+  }
 })
